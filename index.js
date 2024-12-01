f9c9cdc0dd1af6cb889d6e4248eea6063bd3b4aa
josephmode <jjddvv10@gmail.com>
2024-12-01 10:48:39 -0500

    cors updated

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ import MarketRoutes from './Routes/MarketRoutes.js'
 
 const app = express();
 const port = 3001;
+
+const corsOptions = {
+    origin: 'https://sidesoft-test.ddns.net',
+    methods: 'GET,POST',
+    allowedHeaders: 'Content-Type',
+  };
+
 app.use(cors());
 
 app.use(express.json());
